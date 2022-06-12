8ae381478d15feea68fa009fcdff2ddb57d5d73e
hector <crespomhector@gmail.com>
2022-06-12 19:05:05 -0400

    completed styling navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,29 +6,25 @@ const NavBar = () => {
     return (
         <div className="navbar">
 
-            <div className="popular-city">
-                <label className="navbar__label">Popular Cities
-                    <select name="popular cities" className="navbar__select">
-                        <option value="NewYorkCity">New York</option>
-                        <option value="Miami">Miami</option>
-                        <option value="LasVegas">Las Vegas</option>
-                        <option value="Austin">Austin</option>
-                        <option value="Honolulu">Honolulu</option>
-                    </select>
-                </label>
-            </div>
+            <label className="navbar__label">Popular Cities
+                <select name="popular cities" className="navbar__select">
+                    <option value="NewYorkCity">New York</option>
+                    <option value="Miami">Miami</option>
+                    <option value="LasVegas">Las Vegas</option>
+                    <option value="Austin">Austin</option>
+                    <option value="Honolulu">Honolulu</option>
+                </select>
+            </label>
 
-            <div className="new-city">
-                <label className="navbar__label-newcity">Search New City
-                    <label className="navbar__label-latitude">Latitude
-                        <input type="number" name="latitude" />
-                    </label>
-                    <label className="navbar__label-latitude">Longitude
-                        <input type="number" name="longitude" />
-                    </label>
-                    <input className="navbar__input-submit" type="submit" />
+            <label className="navbar__label-newcity">Search New City
+                <label className="navbar__label-latitude">Latitude
+                    <input type="number" name="latitude" />
                 </label>
-            </div>
+                <label className="navbar__label-longitude">Longitude
+                    <input type="number" name="longitude" />
+                </label>
+                <button className="navbar__button-submit" type="button">submit</button>
+            </label>
         </div>
     )
 }
